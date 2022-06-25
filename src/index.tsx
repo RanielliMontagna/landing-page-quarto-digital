005bbe0807f18b3edc781b142ec1556520c44864
Ranielli Montagna <raniellimontagna@hotmail.com>
2022-06-25 14:27:59 -0300

    feat: adiciona service-worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
+
 import { App } from './App';
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 require('dotenv').config();
 
@@ -20,6 +24,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
